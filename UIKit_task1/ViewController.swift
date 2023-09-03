588df7af33fd8fad65566dc3d802d3fbe7af3dc3
roman1pr <proman11pr@gmail.com>
2023-09-03 14:37:50 +0300

    Initial Commit

---
+++ UIKit_task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_task1
+//
+//  Created by Роман Приискалов on 03.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
